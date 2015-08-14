5a5e84c15987479bfd436d14b9a8dba0004bafe0
Pankaj Rawat <pankajrawat19sept@gmail.com>
2015-08-14 20:08:45 +0530

    added comment

--- www/js/controllers/controllers.js
+++ www/js/controllers/controllers.js
@@ -1,11 +1,30 @@
 (function(){
   angular.module('starter')
 
-  .controller('ListController', ['$scope', '$http', '$q', '$state',
-    function ($scope, $http, $q, $state) {
+  .controller('ListController',
+    [
+      '$scope', '$http', '$q', '$state', '$ionicModal', 
+      function ($scope, $http, $q, $state, $ionicModal) {
       // pull data to populate data
-      var remoteDataLink = 'http://beta.json-generator.com/api/json/get/FDUuC2J';
+      //var remoteDataLink = 'http://beta.json-generator.com/api/json/get/FDUuC2J';
+      var remoteDataLink = 'http://localhost:3000/api/v1/reservations';
       console.log('in list controller');
+
+      // Comment Modal Here
+      //--------------------
+        $ionicModal.fromTemplateUrl('/templates/comment_modal.html', {
+          scope: $scope,
+          animation: 'slide-in-up'
+        }).then(function(modal) {
+          $scope.modal = modal;
+        });
+        $scope.openModal = function() {
+          $scope.modal.show();
+        };
+        $scope.closeModal = function() {
+          $scope.modal.hide();
+        };
+      //---------------------
 
       $scope.data = {showDelete: false, showReorder: false};
 
@@ -39,7 +58,8 @@
         $http.get(remoteDataLink)
           .success(function(data){
             console.log('data from server', data);
-            $scope.reservations = data;
+            window.data = data;
+            $scope.reservations = data.reservations;
             deferred.resolve('data recieved');
             showDetail();
           })
@@ -56,6 +76,18 @@
           }
         });
       };
+
+      $scope.postComment = function(){
+        postCommentUrl = "http://localhost:3000/api/v1/reservations/" + $scope.reservation.id + "/comments";
+        $http.post(postCommentUrl,{message: $scope.reservation.new_comment})
+          .success(function(data){
+            $scope.reservation.comments.push(data['comment']);
+            $scope.reservation.new_comment = '';
+          })
+          .error(function(err){
+            console.log(err);     
+          })
+      }
   }])
 
 })();
